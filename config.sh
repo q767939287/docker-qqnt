44839136b01592f3ea2fb38888c0f86c32db06a2
桜咲 <767939287@qq.com>
2023-11-05 09:04:51 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,4 @@
 BUILD_QQNT_DATA_LIST=(
   # 顺序为从新到旧，最新版在最上方
-  3.2.1-17816,https://dldir1.qq.com/qqfile/qq/QQNT/4cb54015/linuxqq_3.2.1-17816
-  3.1.1-11223,https://dldir1.qq.com/qqfile/qq/QQNT/2355235c/linuxqq_3.1.1-11223
-  3.1.2-12912,https://dldir1.qq.com/qqfile/qq/QQNT/80d33f88/linuxqq_3.1.2-12912
+  3.2.2-18163,https://dldir1.qq.com/qqfile/qq/QQNT/d264d6e3/linuxqq_3.2.2-18163
 )
